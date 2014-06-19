be55030943e7e11d369db515c6897b017d335123
Trond Aarø <trond@itrond.net>
2014-06-19 07:58:41 +0200

    changed to explisit body-parser

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 var fs = require('fs');
 
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 var comments = [{author: 'Pete Hunt', text: 'Hey there!'}];
 
 app.use('/', express.static(__dirname));
-app.use(express.bodyParser());
+app.use(bodyParser);
 
 app.get('/comments.json', function(req, res) {
   res.setHeader('Content-Type', 'application/json');
@@ -19,4 +20,6 @@ app.post('/comments.json', function(req, res) {
   res.send(JSON.stringify(comments));
 });
 
-app.listen(3000);
+var port = 3000;
+app.listen(port);
+console.log("listen on port " + port);
