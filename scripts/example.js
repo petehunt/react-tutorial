0f343056492c97a122b784a6a858407114f588c3
Cheng Lou <chenglou92@gmail.com>
2014-06-29 21:33:17 -0700

    Put tutorial code up-to-date
    - Use React 0.10
    - Reach line-by-line parity with tutorial code.
    - Use showdown CDN like in the tutorial.
    - New jQuery version to supress the weird "JSON syntax error" from 1.10.
    - Fix a rare race condition where the optimistically updated comments is set after the ajax request succeeds.
    - Lock express version (new one drops Connect, so the `node server.js` command fails.

--- scripts/example.js
+++ scripts/example.js
@@ -7,7 +7,9 @@ var Comment = React.createClass({
     var rawMarkup = converter.makeHtml(this.props.children.toString());
     return (
       <div className="comment">
-        <h2 className="commentAuthor">{this.props.author}</h2>
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
         <span dangerouslySetInnerHTML={{__html: rawMarkup}} />
       </div>
     );
@@ -18,22 +20,34 @@ var CommentBox = React.createClass({
   loadCommentsFromServer: function() {
     $.ajax({
       url: this.props.url,
+      dataType: 'json',
       success: function(data) {
         this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
       }.bind(this)
     });
   },
   handleCommentSubmit: function(comment) {
     var comments = this.state.data;
     comments.push(comment);
-    this.setState({data: comments});
-    $.ajax({
-      url: this.props.url,
-      type: 'POST',
-      data: comment,
-      success: function(data) {
-        this.setState({data: data});
-      }.bind(this)
+    this.setState({data: comments}, function() {
+      // `setState` accepts a callback. To avoid (improbable) race condition,
+      // `we'll send the ajax request right after we optimistically set the new
+      // `state.
+      $.ajax({
+        url: this.props.url,
+        dataType: 'json',
+        type: 'POST',
+        data: comment,
+        success: function(data) {
+          this.setState({data: data});
+        }.bind(this),
+        error: function(xhr, status, err) {
+          console.error(this.props.url, status, err.toString());
+        }.bind(this)
+      });
     });
   },
   getInitialState: function() {
@@ -56,10 +70,21 @@ var CommentBox = React.createClass({
 
 var CommentList = React.createClass({
   render: function() {
-    var commentNodes = this.props.data.map(function (comment, index) {
-      return <Comment key={index} author={comment.author}>{comment.text}</Comment>;
+    var commentNodes = this.props.data.map(function(comment, index) {
+      return (
+        // `key` is a React-specific concept and is not mandatory for the
+        // purpose of this tutorial. if you're curious, see more here:
+        // http://facebook.github.io/react/docs/multiple-components.html#dynamic-children
+        <Comment author={comment.author} key={index}>
+          {comment.text}
+        </Comment>
+      );
     });
-    return <div className="commentList">{commentNodes}</div>;
+    return (
+      <div className="commentList">
+        {commentNodes}
+      </div>
+    );
   }
 });
 
@@ -84,6 +109,6 @@ var CommentForm = React.createClass({
 });
 
 React.renderComponent(
-  <CommentBox url="/comments.json" pollInterval={2000} />,
-  document.getElementById('container')
+  <CommentBox url="comments.json" pollInterval={2000} />,
+  document.getElementById('content')
 );
